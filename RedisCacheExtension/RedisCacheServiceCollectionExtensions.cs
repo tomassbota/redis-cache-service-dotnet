69aae67771c3d9f8d64a11f0091bf12c3a034ecc
Tomás Bota <tbota@mainhub.pt>
2024-05-02 16:18:21 +0100

    First Commit, added project

---
+++ RedisCacheExtension/RedisCacheServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Microsoft.Extensions.DependencyInjection;
+using RedisCacheExtension.Interfaces;
+using RedisCacheExtension.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisCacheExtension
+{
+    public static class RedisCacheServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRedisCache(this IServiceCollection services, Action<RedisCacheOptions> configureOptions)
+        {
+            services.AddStackExchangeRedisCache(configureOptions);
+
+            services.AddScoped<IRedisCacheService, RedisCacheService>();
+
+            return services;
+        }
+    }
+}
---
+++ RedisCacheExtension/Services/RedisCacheService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+using RedisCacheExtension.Interfaces;
+
+namespace RedisCacheExtension.Services
+{
+    public class RedisCacheService : IRedisCacheService
+    {
+        private readonly IDistributedCache? _cache;
+
+        public RedisCacheService(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public void SetData<T>(string key, T element, TimeSpan cacheDuration)
+        {
+            var json = JsonSerializer.Serialize(element);
+
+            _cache!.SetString(key, json, new() { AbsoluteExpirationRelativeToNow = cacheDuration });
+        }
+
+        public T GetData<T>(string key)
+        {
+            var data = _cache!.GetString(key) ?? string.Empty;
+
+            if (data == string.Empty)
+            {
+                return default(T)!;
+            }
+
+            return JsonSerializer.Deserialize<T>(data)!;
+        }
+
+    }
+}
